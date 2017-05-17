982b00f79e24e9d6483778cc1a4c4f160d3bf405
PavelProtchenko <protchenkopa@gmail.com>
2017-05-18 01:59:11 +0300

    add validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
 
   def show
-    @hello = "Привет, Паша!"
+    @user = User.new(
+      name: 'Vadim',
+      username: 'installero',
+      avatar_url: 'https://secure.gravatar.com/avatar/7126968e0f757ddb4f73614f43ae445?s=100'
+    )
   end
 end
