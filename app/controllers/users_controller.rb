ccd3531efec13b90a392928749aa2b64de5de400
PavelProtchenko <protchenkopa@gmail.com>
2017-05-26 03:39:40 +0300

    helper_partial_css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,5 +14,10 @@ class UsersController < ApplicationController
       username: 'installero',
       avatar_url: 'https://secure.gravatar.com/avatar/7126968e0f757ddb4f73614f43ae445?s=100'
     )
+
+    @questions = [
+        Question.new(text: 'Как жизнь?', created_at: Date.parse('26.05.2017')),
+        Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('26.05.2017'))
+    ]
   end
 end
