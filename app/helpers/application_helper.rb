a413a61f81fbde2960493f984ec516d4edfa0e65
PavelProtchenko <protchenkopa@gmail.com>
2017-06-01 22:29:05 +0300

    add declansion to questions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,19 @@ module ApplicationHelper
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
+
+  def declansion(number, krokodil, krokodila, krokodilov)
+    if number % 100 >= 5 && number % 100 <= 20
+      krokodilov
+
+    elsif number % 10 == 1
+      krokodil
+
+    elsif number % 10 >= 2 && number % 10 <= 4
+      krokodila
+
+    else
+      krokodilov
+    end
+  end
 end
