667e856c127a6fb9f1c59d3be14317c18234bf16
PavelProtchenko <protchenkopa@gmail.com>
2017-07-30 21:34:20 +0300

    change sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.authenticate(params[:email], params[:password])
-    user = User.authenticate(params[:email], params[:password])
 
     if @user.present?
       session[:user_id] = @user.id
